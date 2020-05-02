6ebac6d5c619f3f19bc247fcaa19cdcdfc9367d8
Azeem Uraizee <aauraizee@gmail.com>
2020-05-01 20:37:24 -0400

    Additional folder structure udpates. Initialized GameViewer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import GameViewer from './components/GameViewer/GameViewer';
+
 class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>Bowling Tracker</h1>
+                <GameViewer />
             </div>
         )
     }
---
+++ src/components/GameViewer/GameViewer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const GameViewer = () => {
+    return (
+        <h1>GameViewer</h1>
+    )
+}
+
+export default GameViewer;
