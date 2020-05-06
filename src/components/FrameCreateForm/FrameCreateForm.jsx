60bdf9a29a788c7120d54be757ae501e33aa742a
Azeem Uraizee <aauraizee@gmail.com>
2020-05-06 15:00:49 -0400

    Made form a multi-step form. Functionality to pass data up to parent form on next

--- src/components/FrameCreateForm/FrameCreateForm.jsx
+++ src/components/FrameCreateForm/FrameCreateForm.jsx
@@ -1,13 +1,154 @@
-import React from 'react';
-import { Card, CardContent, Typography } from '@material-ui/core';
+import React, { useState, useEffect, Fragment } from 'react';
+import { Card, CardContent, Typography, FormControl, InputLabel, Select, TextField, MenuItem, Grid } from '@material-ui/core';
 
-const FrameCreateForm = () => {
+import { MobileStepper, Button } from '@material-ui/core';
+import { KeyboardArrowRight, KeyboardArrowLeft } from '@material-ui/icons'
+
+const FrameCreateForm = (props) => {
+
+    const { number } = props;
+
+    const [activeStep, setActiveStep] = useState(number)
+    const [frameType, setFrameType] = useState('')
+    const [frameData, setFrameData] = useState({ num: activeStep+1, type: frameType })
+
+    useEffect(() => {
+        if (frameType === 0) {
+            setFrameData({ num: activeStep+1, type: frameType, shot1: 10 })
+        } else if (frameType === 1 || frameType === 2) {
+            setFrameData({ num: activeStep+1, type: frameType, shot1: 0, shot2: 0, spareType: '' })
+        } else if (frameType === 3) {
+            setFrameData({ num: activeStep+1, type: frameType, shot1: 0, shot2: 0, shot3: 0 })
+        }
+        console.log('rerender')
+    }, [frameType]);
+
+    const handleSelectChange = (e) => {
+        setFrameType(e.target.value);
+    }
+
+    const handleChange = (e) => {
+        setFrameData({
+            ...frameData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleNext = () => {
+        const frameAdded = props.onNext(frameData)
+        if(frameAdded) {
+            setActiveStep((activeStep) => activeStep + 1)
+        }  
+    }
+
+    const handleBack = () => {
+        setActiveStep((activeStep) => activeStep - 1)
+    }
+
+    const buildTextFields = (frameType) => {
+        switch (frameType) {
+            case 0:
+                return (
+                    <Grid item xs={12}>
+                        <Typography variant='h6' align='center'>X</Typography>
+                    </Grid>  
+                )
+            case 1:
+                return (
+                    <Fragment>
+                        <Grid item xs={6}>
+                            <TextField id="shot-1" label="Shot 1" variant='outlined' margin='dense' name='shot1' onChange={handleChange} />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Typography variant='h6' align='center'>/</Typography>
+                        </Grid> 
+                        <Grid item xs={12}>
+                            <TextField id='spare-type' label="Enter Spare Type" margin='dense' name='spareType' onChange={handleChange} />
+                        </Grid>   
+                    </Fragment>
+                )
+            case 2:
+                return (
+                    <Fragment>
+                        <Grid item xs={6}>
+                            <TextField id="shot-1" label="Shot 1" variant='outlined' margin='dense' name='shot1' onChange={handleChange}  />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField id="shot-2" label="Shot 2" variant='outlined' margin='dense' name='shot2' onChange={handleChange} />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField id='spareType' label="Enter Spare Type" margin='dense' name='spareType' onChange={handleChange}>/</TextField>
+                        </Grid> 
+                    </Fragment>
+                )
+            case 3:
+                return (
+                    <Fragment>
+                        <Grid item xs={4}>
+                            <TextField id="shot-1" label="Shot 1" variant='outlined' margin='dense' name='shot1' onChange={handleChange} />
+                        </Grid>
+                        <Grid item xs={4}>
+                            <TextField id="shot-2" label="Shot 2" variant='outlined' margin='dense' name='shot2' onChange={handleChange} />
+                        </Grid>
+                        <Grid item xs={4}>
+                            <TextField id="shot-3" label="Shot 3" variant='outlined' margin='dense' name='shot3' onChange={handleChange} />
+                        </Grid>
+                    </Fragment>
+                )
+            default:
+                return (
+                    <Grid item xs={12}>
+                        <Typography variant='subtitle2' align='center'>Select a frame type</Typography>
+                    </Grid> 
+                )
+        }
+    }
+
     return (
+        <Fragment>
         <Card variant='outlined'>
             <CardContent>
-                <Typography variant='overline' align='left'>Frame ?</Typography>
+                <Typography variant='overline' align='left'>Frame {activeStep+1}</Typography>
+                <Grid container spacing={2} justify='center'>
+                    <Grid item xs={12}>
+                        <FormControl fullWidth margin='dense'>
+                            <InputLabel id="shot-type-select-label">Frame Type</InputLabel>
+                            <Select 
+                                labelId="shot-type-select-label" 
+                                id="shot-type-select"
+                                value={frameType}
+                                onChange={handleSelectChange}
+                            >
+                                <MenuItem value={0}>Strike</MenuItem>
+                                <MenuItem value={1}>Spare</MenuItem>
+                                <MenuItem value={2}>Open</MenuItem>
+                                <MenuItem value={3}>Tenth</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Grid>
+                    {buildTextFields(frameType)}
+                </Grid>
             </CardContent>
         </Card>
+        <MobileStepper
+        variant="progress"
+        steps={10}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+            <Button size="small" onClick={handleNext} disabled={activeStep === 9}>
+            Next
+            <KeyboardArrowRight />
+            </Button>
+        }
+        backButton={
+            <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            <KeyboardArrowLeft />
+            Back
+            </Button>
+        }
+    />
+    </Fragment>
     )
        
     
--- src/components/GameCreateForm/GameCreateForm.jsx
+++ src/components/GameCreateForm/GameCreateForm.jsx
@@ -1,45 +1,59 @@
-import React, { Fragment } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import FrameCreateForm from '../FrameCreateForm/FrameCreateForm';
 
-import { Grid } from '@material-ui/core';
-
 const GameCreateForm = () => {
+
+    const [frameInfo, setFrameInfo] = useState([]);
+    const [shotInfo, setShotInfo] = useState([]);
+    const [activeStep, setActiveStep] = useState(0);
+
+    // useEffect(() => {
+    //     for (let i = 1; i <= 10; i++) {
+    //         let nextFrame = { num: i, type: '' }
+    //         setFrameInfo(frameInfo => [...frameInfo, nextFrame])
+    //     }
+    // }, []);
+
+    const buildStepContent = () => {
+        switch(activeStep) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+                return (<FrameCreateForm number={activeStep} onNext={handleNext} onBack={handleBack} />)    
+        }
+    }
+
+    const handleNext = (data) => {
+        if(frameInfo.some(frame => frame.num === data.num))
+            return true
+        else {
+            setFrameInfo([...frameInfo, data])
+            return true
+        }
+            
+    }
+
+    const handleBack = (data) => {
+        console.log('parent received:' + JSON.stringify(data))
+    }
+
+
     return (
         <Fragment>
-            <Grid container spacing={2} justify="center">
-                <Grid item xs={3}>
-                    <FrameCreateForm />
-                </Grid>
-                <Grid item xs={3}>
-                    <FrameCreateForm />
-                </Grid>
-                <Grid item xs={3}>
-                    <FrameCreateForm />
-                </Grid>
-                <Grid item xs={3}>
-                    <FrameCreateForm />
-                </Grid>
-                <Grid item xs={3}>
-                    <FrameCreateForm />
-                </Grid>
-                <Grid item xs={3}>
-                    <FrameCreateForm />
-                </Grid>
-                <Grid item xs={3}>
-                    <FrameCreateForm />
-                </Grid>
-                <Grid item xs={3}>
-                    <FrameCreateForm />
-                </Grid>
-                <Grid item xs={3}>
-                    <FrameCreateForm />
-                </Grid>
-                <Grid item xs={3}>
-                    <FrameCreateForm />
-                </Grid>
-            </Grid>
+            <form>
+                {buildStepContent()}
+            </form>
         </Fragment>
     )
 }
+
+
 
 export default GameCreateForm;
