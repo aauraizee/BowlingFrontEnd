ec5c6a0df294537ee2d2ce572fb87d0ba72bde24
Azeem Uraizee <aauraizee@gmail.com>
2020-05-13 19:11:22 -0400

    Added preliminary form validation

--- src/components/FrameCreateForm/FrameCreateForm.jsx
+++ src/components/FrameCreateForm/FrameCreateForm.jsx
@@ -1,28 +1,73 @@
 import React, { useState, useEffect, Fragment } from 'react';
-import { Card, CardContent, Typography, FormControl, InputLabel, Select, TextField, MenuItem, Grid } from '@material-ui/core';
+import { Card, CardContent, Typography, FormControl, FormHelperText, InputLabel, Select, TextField, MenuItem, Grid } from '@material-ui/core';
 
 import { MobileStepper, Button } from '@material-ui/core';
 import { KeyboardArrowRight, KeyboardArrowLeft } from '@material-ui/icons'
 
-import {FrameObject} from '../FrameCreateForm/FrameObject';
+import { FrameObject } from '../FrameCreateForm/FrameObject';
+import { FrameErrorObject } from '../FrameCreateForm/FrameErrorObject';
 
 const FrameCreateForm = (props) => {
+
+    const spareTypeRegex = RegExp('\b[0-9]+(\-[0-9]+)*(?!\-)\b')
 
     const [activeStep, setActiveStep] = useState(0)
     const [frameType, setFrameType] = useState('')
     const [frameData, setFrameData] = useState([Object.assign({}, FrameObject)])
+    const [frameErrorData, setFrameErrorData] = useState([Object.assign({}, FrameErrorObject)])
+    
 
     const handleChange = (e) => {
         var fd = frameData;
+        var fde = frameErrorData;
         if(e.target.name === "type" && activeStep === 9) { return }
-        fd[activeStep][e.target.name] = e.target.value;
-
-        setFrameData([...frameData])
 
         if(e.target.name === "type") {
             if (activeStep === 9) { return }
             setFrameType(e.target.value)
+
+            fd[activeStep] = Object.assign({}, {
+                num: activeStep + 1,
+                type: e.target.value,
+                shot1: 0,
+                shot2: 0,
+                shot3: 0,
+                spareType: ""
+            })
+            fde[activeStep] = Object.assign({}, FrameErrorObject)
+            setFrameData([...frameData])
+            setFrameErrorData([...frameErrorData])
+        } else {
+            fd[activeStep][e.target.name] = e.target.value;
         }
+
+        
+        setFrameData([...frameData])
+
+        // if(e.target.name === "type") {
+        //     if (activeStep === 9) { return }
+        //     setFrameType(e.target.value)
+        // }
+
+        var fdErrors = frameErrorData;
+        switch (e.target.name) {
+            case "shot1":
+            case "shot2":
+            case "shot3":
+                fdErrors[activeStep][e.target.name] =
+                    Number(e.target.value) > 10 || Number(e.target.value) < 0 && e.target.value.length > 0
+                    ? "Shot value must be between 0 and 10"
+                    : ""
+                break
+            case "type":
+                fdErrors[activeStep][e.target.name] =
+                    e.target.value === 3 && activeStep !== 9
+                    ? "Only the 10th frame can be type Tenth"
+                    : ""
+                break   
+        }
+        setFrameErrorData([...frameErrorData])
+
     }
 
     const saveToLocal = () =>{
@@ -50,7 +95,8 @@ const FrameCreateForm = (props) => {
                         shot3: 0,
                         spareType: ""
                     })])
-                    setFrameType(3) 
+                    setFrameType(3)
+                    setFrameErrorData([...frameErrorData, Object.assign({}, FrameErrorObject)]) 
                 } else {
                     setFrameData([...frameData, Object.assign({}, {
                         num: activeStep + 2,
@@ -60,7 +106,8 @@ const FrameCreateForm = (props) => {
                         shot3: 0,
                         spareType: ""
                     })])
-                    setFrameType('') 
+                    setFrameType('')
+                    setFrameErrorData([...frameErrorData, Object.assign({}, FrameErrorObject)])  
                 }
             } else {
                 setFrameType(frameData[activeStep+1].type)
@@ -78,11 +125,28 @@ const FrameCreateForm = (props) => {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log("Submitting data")
+        let isValid = validate()
+       
+
+    }
+
+    const validate = () => {
+        let isValid = true
+        frameErrorData.map(frame => {
+            let errors = Object.keys(frame)
+            for (var i = 0; i < errors.length; i++) {
+                if (frame[errors[i]].length > 0) {
+                    isValid = false;
+                    break
+                }
+            }
+        })
+        return isValid
     }
 
     const buildTextFields = (frameType) => {
         const currentFrameObject = frameData[activeStep]
+        const currentErrorObject = frameErrorData[activeStep]
         switch (currentFrameObject.type) {
             case 0:
                 return (
@@ -94,7 +158,8 @@ const FrameCreateForm = (props) => {
                 return (
                     <Fragment>
                         <Grid item xs={6}>
-                            <TextField id="shot-1" label="Shot 1" variant='outlined' margin='dense' name='shot1' value={currentFrameObject.shot1} type='number' onChange={handleChange} />
+                            <TextField id="shot-1" label="Shot 1" variant='outlined' margin='dense' name='shot1' value={currentFrameObject.shot1} type='number' 
+                                       onChange={handleChange} helperText={currentErrorObject['shot1']} error={currentErrorObject['shot1'].length > 0} />
                         </Grid>
                         <Grid item xs={6}>
                             <Typography variant='h6' align='center'>/</Typography>
@@ -108,10 +173,12 @@ const FrameCreateForm = (props) => {
                 return (
                     <Fragment>
                         <Grid item xs={6}>
-                            <TextField id="shot-1" label="Shot 1" variant='outlined' margin='dense' name='shot1' value={currentFrameObject.shot1} type='number' onChange={handleChange}  />
+                            <TextField id="shot-1" label="Shot 1" variant='outlined' margin='dense' name='shot1' value={currentFrameObject.shot1} type='number' 
+                                       onChange={handleChange} helperText={currentErrorObject['shot1']} error={currentErrorObject['shot1'].length > 0}  />
                         </Grid>
                         <Grid item xs={6}>
-                            <TextField id="shot-2" label="Shot 2" variant='outlined' margin='dense' name='shot2' value={currentFrameObject.shot2} type='number' onChange={handleChange} />
+                            <TextField id="shot-2" label="Shot 2" variant='outlined' margin='dense' name='shot2' value={currentFrameObject.shot2} type='number' 
+                                       onChange={handleChange} helperText={currentErrorObject['shot2']} error={currentErrorObject['shot2'].length > 0} />
                         </Grid>
                         <Grid item xs={12}>
                             <TextField id='spareType' label="Enter Spare Type" margin='dense' name='spareType' value={currentFrameObject.spareType} onChange={handleChange}>/</TextField>
@@ -122,13 +189,16 @@ const FrameCreateForm = (props) => {
                 return (
                     <Fragment>
                         <Grid item xs={4}>
-                            <TextField id="shot-1" label="Shot 1" variant='outlined' margin='dense' name='shot1' value={currentFrameObject.shot1} type='number' onChange={handleChange} />
+                            <TextField id="shot-1" label="Shot 1" variant='outlined' margin='dense' name='shot1' value={currentFrameObject.shot1} type='number' 
+                                       onChange={handleChange} helperText={currentErrorObject['shot1']} error={currentErrorObject['shot1'].length > 0} />
                         </Grid>
                         <Grid item xs={4}>
-                            <TextField id="shot-2" label="Shot 2" variant='outlined' margin='dense' name='shot2' value={currentFrameObject.shot2} type='number' onChange={handleChange} />
+                            <TextField id="shot-2" label="Shot 2" variant='outlined' margin='dense' name='shot2' value={currentFrameObject.shot2} type='number' 
+                                       onChange={handleChange} helperText={currentErrorObject['shot2']} error={currentErrorObject['shot2'].length > 0} />
                         </Grid>
                         <Grid item xs={4}>
-                            <TextField id="shot-3" label="Shot 3" variant='outlined' margin='dense' name='shot3' value={currentFrameObject.shot3} type='number' onChange={handleChange} />
+                            <TextField id="shot-3" label="Shot 3" variant='outlined' margin='dense' name='shot3' value={currentFrameObject.shot3} type='number' 
+                                       onChange={handleChange} helperText={currentErrorObject['shot3']} error={currentErrorObject['shot3'].length > 0} />
                         </Grid>
                     </Fragment>
                 )
@@ -148,7 +218,7 @@ const FrameCreateForm = (props) => {
                 <Typography variant='overline' align='left'>Frame {activeStep+1}</Typography>
                 <Grid container spacing={2} justify='center'>
                     <Grid item xs={12}>
-                        <FormControl fullWidth margin='dense'>
+                        <FormControl fullWidth margin='dense' error={frameErrorData[activeStep]['type'].length > 0}>
                             <InputLabel id="shot-type-select-label">Frame Type</InputLabel>
                             <Select 
                                 labelId="shot-type-select-label" 
@@ -162,6 +232,7 @@ const FrameCreateForm = (props) => {
                                 <MenuItem value={2}>Open</MenuItem>
                                 <MenuItem value={3}>Tenth</MenuItem>
                             </Select>
+                            <FormHelperText>{frameErrorData[activeStep]['type']}</FormHelperText>
                         </FormControl>
                     </Grid>
                     {buildTextFields(frameType)}
---
+++ src/components/FrameCreateForm/FrameErrorObject.jsx
@@ -0,0 +1,8 @@
+export const FrameErrorObject = {
+    num: "",
+    type: "",
+    shot1: "",
+    shot2: "",
+    shot3: "",
+    spareType: ""
+}
